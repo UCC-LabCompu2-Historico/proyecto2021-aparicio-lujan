c3df4eb5fb24e3c129c9982f8d2dea28f64dd1bb
Lujham <lautiRlujan@gmail.com>
2021-07-26 05:54:00 -0300

    se le dio movimiento al player (da error)

--- juego.js
+++ juego.js
@@ -1,18 +1,15 @@
-
 var edad;
 function cargarWeb(){
-
   var nom, urlGame;
   nom=document.getElementById("Name").value
   edad=document.getElementById("Difi").value
-alert(edad);
-
-
+  alert(edad);
   urlGame = "game.html#" + nom + "#" + edad;
   if(nom!==""){
     window.open(urlGame);
   }else alert("Ingrese un Nombre Por Favor");
 }
+
 function J(){
   var urlGame, nom, difi;
   urlGame = window.location.href.split("/")[4];
@@ -21,7 +18,6 @@ function J(){
   document.getElementById("Snombre").innerHTML = nom;
   document.getElementById("Sdifi").innerHTML = difi;
 }
-
 
 function armarPantalla() {
   const canvas = document.getElementById("game-canvas");
@@ -43,7 +39,6 @@ function dibujarFondo(ctx) {
     ctx.drawImage(fondo, 0, 0)
   };
   fondo.src = "Imagenes/estrellas.png";
-
 }
 
 function dibujarScore(ctx) {
@@ -66,7 +61,6 @@ function dibujarEnemigos(ctx) {
   const invasor = document.createElement("img");
   invasor.src = "Imagenes/inva2.png";
   if (edad === 'hard') {
-
     invasor.onload = function () {
       for (let i = 0; i < 5; i++) {
         let j = i * 200;
@@ -83,26 +77,29 @@ function dibujarEnemigos(ctx) {
         ctx.drawImage(invasor, (200 + j), 100);
         ctx.drawImage(invasor, (200 + j), 200);
         ctx.drawImage(invasor, (200 + j), 300);
-
       }
     }
   }
+}
 /* ************************************
     Funciones de Jugador/Player
  *************************************/
 
 //width="1200" height="800"
+const KEY_CODE_LEFT= 37;
+const KEY_CODE_RIGHT= 39;
+const KEY_CODE_X= 88;
 
-// const GAME_STATE = {
-//   playerX: 0,
- // playerY: 0,
+const GAME_STATE = {
+  playerX: 0,
+  playerY: 0,
 }
 
-//function setPosition($el, x, y){
- // $el.style.transform = `translate(${x}px, ${y}px)`;
-//}
+function setPosition($el, x, y){
+  $el.style.transform = `translate(${x}px, ${y}px)`;
+}
 
-/*function createPlayer ($container){
+function dibujarJugador ($container){
   GAME_STATE.playerX = 1200/2;
   GAME_STATE.playerY = 800 - 50;
   const $player = document.createElement("img");
@@ -113,10 +110,25 @@ function dibujarEnemigos(ctx) {
 }
 function init(){
   const $container = document.querySelector(".juego");
-  createPlayer ($container);
-}*/
-//init();
-//}
+  dibujarJugador ($container);
+}
+
+function onKeyDown(e){
+  if (e.keyCode === KEY_CODE_LEFT){
+    GAME_STATE.playerX -= 5;
+    const $player = document.querySelector('.player');
+    setPosition($player, GAME_STATE.playerX, GAME_STATE.playerY);
+  }else if (e.keyCode === KEY_CODE_RIGHT){
+    GAME_STATE.playerX += 5;
+    const $player = document.querySelector('.player');
+    setPosition($player, GAME_STATE.playerX, GAME_STATE.playerY);
+  }
+}
+init();
+window.addEventListener("keydown",onKeyDown);
+
+
+
 
 
 
